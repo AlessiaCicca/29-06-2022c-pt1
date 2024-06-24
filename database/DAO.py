ee257310d23b571db733d9d9b705b28c9ff7fba3
Alessia Ciccaglione <s295431@studenti.polito.it>
2024-06-24 19:15:38 +0200

    attenzione usa round

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,56 @@
 from database.DB_connect import DBConnect
-
+from model.album import Album
+from model.connessione import Connessione
 
 class DAO():
     def __init__(self):
         pass
+
+    @staticmethod
+    def getNodi(prezzo):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select a.*
+from album a ,track t 
+where a.AlbumId=t.AlbumId 
+group by t.AlbumId 
+having sum(t.UnitPrice)>%s"""
+
+        cursor.execute(query,(prezzo,))
+
+        for row in cursor:
+            result.append(Album(**row))
+
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def getConnessioni(n):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select t1.a1 as v1, t2.a2 as v2, round(t1.p1-t2.p2) as peso
+from(select t.AlbumId as a1, sum(t.UnitPrice) as p1
+from track t 
+group by t.AlbumId 
+having sum(t.UnitPrice)>%s) as t1,
+(select t.AlbumId as a2, sum(t.UnitPrice) as p2
+from track t 
+group by t.AlbumId 
+having sum(t.UnitPrice)>%s) as t2
+where t1.a1<t2.a2  """
+
+        cursor.execute(query,(n,n,))
+
+        for row in cursor:
+            result.append(Connessione(**row))
+
+        cursor.close()
+        conn.close()
+        return result
--- model/model.py
+++ model/model.py
@@ -1,3 +1,66 @@
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self.grafo = nx.DiGraph()
+        self._idMap = {}
+        self._idMapNome = {}
+
+    def creaGrafo(self, prezzo):
+        self.nodi = DAO.getNodi(prezzo)
+        self.grafo.add_nodes_from(self.nodi)
+        for v in self.nodi:
+            self._idMap[v.AlbumId] = v
+        for v in self.nodi:
+            self._idMapNome[v.Title] = v
+        self.addEdges(prezzo)
+        return self.grafo
+
+    def getNumNodes(self):
+        return len(self.grafo.nodes)
+
+    def getNumEdges(self):
+        return len(self.grafo.edges)
+
+    def addEdges(self, prezzo):
+        self.grafo.clear_edges()
+        allEdges = DAO.getConnessioni(prezzo)
+        for connessione in allEdges:
+            nodo1 = self._idMap[connessione.v1]
+            nodo2 = self._idMap[connessione.v2]
+            if nodo1 in self.grafo.nodes and nodo2 in self.grafo.nodes and nodo1!=nodo2:
+                if self.grafo.has_edge(nodo1, nodo2) == False:
+                    if connessione.peso>0:
+                        self.grafo.add_edge(nodo1, nodo2, weight=connessione.peso)
+                    if connessione.peso<0:
+                        self.grafo.add_edge(nodo2, nodo1, weight=abs(connessione.peso))
+
+    def getBilancio(self, a1Titolo):
+        album=self._idMapNome[a1Titolo]
+        lista=[]
+        print(album.AlbumId)
+        for nodo in self.grafo.successors(album):
+            print("ciao")
+            lista.append((nodo.Title,self.bilancio(nodo)))
+            print(nodo)
+        for nodo in self.grafo.predecessors(album):
+            print("ciao")
+            lista.append((nodo.Title,self.bilancio(nodo)))
+            print(nodo)
+        return sorted(lista, key=lambda x:x[1], reverse=True)
+
+
+    def bilancio(self,nodo):
+        print(nodo)
+        numeroArchi=self.grafo.degree(nodo)
+        somma=0
+        for arco in self.grafo.in_edges(nodo):
+            somma+=self.grafo[arco[0]][arco[1]]["weight"]
+        for arco in self.grafo.out_edges(nodo):
+            somma+=self.grafo[arco[0]][arco[1]]["weight"]
+        print(somma)
+        print(numeroArchi)
+        return somma/numeroArchi
